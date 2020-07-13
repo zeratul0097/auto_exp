f8f6785a48a90e06a863beb4da4e287ca8c7e52c
daoducminh <daoducminh1997@gmail.com>
2020-07-13 21:35:46 +0700

    Optimize crawler for genre list

--- auto_exp/constants/yy_constants.py
+++ auto_exp/constants/yy_constants.py
@@ -5,6 +5,7 @@ BASE_URL = 'https://truyenyy.com/truyen/{}/danh-sach-chuong?nf=yes'
 # Book's info
 BOOK_URL = '//a[starts-with(@href,"/truyen/")]/@href'
 BOOK_LAST_CHAPTER_PATH = "//table[contains(@class,'table table-dark')]/tbody/tr[1]/td[1]/a/@href"
+PREMIUM_TAG_PATH = "//a[contains(@href,'/chuong-vip')and @class='btn btn-warning']"
 
 # Chapter's content
 CHAPTER_URL = 'https://truyenyy.com/truyen/{0}/chuong-{1}.html'
@@ -17,3 +18,5 @@ TOP_BOOK_TIME_URL = 'https://truyenyy.com/kim-thanh-bang/?year={0}&month={1}&pag
 # Genre
 GENRE_URL = '//a[@class="genre" and starts-with(@href,"/truyen-")]/@href'
 GENRE_PAGE_URL = 'https://truyenyy.com/{0}/danh-sach/?page={1}'
+GENRE_LIST_URL = 'https://truyenyy.com/{0}/danh-sach/'
+GENRE_LIST_PAGINATION_PATH = "//ul[contains(@class,'pagination')]//a[@class='page-link']/@href"
--- auto_exp/spiders/yy.py
+++ auto_exp/spiders/yy.py
@@ -35,13 +35,13 @@ class YYSpider(Spider):
             callback=self.parse_main_page
         )
         # Crawl all top-book time
-        for year in range(2018, 2021):
-            for month in range(1, 13):
-                for page in range(1, 41):
-                    yield Request(
-                        url=TOP_BOOK_TIME_URL.format(year, month, page),
-                        callback=self.parse_top_books
-                    )
+        # for year in range(2018, 2021):
+        #     for month in range(1, 13):
+        #         for page in range(1, 41):
+        #             yield Request(
+        #                 url=TOP_BOOK_TIME_URL.format(year, month, page),
+        #                 callback=self.parse_top_books
+        #             )
 
     def parse_top_books(self, response):
         url_lists = response.xpath(TOP_BOOK_URLS)
@@ -65,16 +65,47 @@ class YYSpider(Spider):
             )
         genre_urls = [x.get() for x in genre_urls]
         genres = list(set([x.replace('/', '') for x in genre_urls]))
-        for k in range(len(genres)):
+        for k in range(2):
             yield Request(
                 url=response.urljoin(genre_urls[k]),
                 callback=self.parse_books_in_page
             )
-            for i in range(1, 250):
-                yield Request(
-                    url=GENRE_PAGE_URL.format(genres[k], i),
-                    callback=self.parse_top_books
-                )
+            yield Request(
+                url=GENRE_LIST_URL.format(genres[k]),
+                callback=self.parse_genre_list,
+                cb_kwargs=dict(genre=genres[k])
+            )
+            # for i in range(1, 250):
+            #     yield Request(
+            #         url=GENRE_PAGE_URL.format(genres[k], i),
+            #         callback=self.parse_top_books
+            #     )
+
+    def parse_genre_list(self, response, genre):
+        page_urls = response.xpath(GENRE_LIST_PAGINATION_PATH)
+        page_url = page_urls[-2].get()
+        last_page = 250
+
+        # Copied from parse_top_books
+        url_lists = response.xpath(TOP_BOOK_URLS)
+        for url in url_lists:
+            short_name = [x for x in url.get().split('/') if x][-1]
+            yield Request(
+                url=BASE_URL.format(short_name),
+                callback=self.parse_book_info,
+                cb_kwargs=dict(short_name=short_name)
+            )
+
+        # Yield request for pages from 2 to last
+        try:
+            last_page = int(page_url[6:])
+        except:
+            pass
+        for i in range(2, last_page + 1):
+            yield Request(
+                url=GENRE_PAGE_URL.format(genre, i),
+                callback=self.parse_top_books
+            )
 
     def parse_books_in_page(self, response):
         book_urls = response.xpath(BOOK_URL)
@@ -87,21 +118,23 @@ class YYSpider(Spider):
             )
 
     def parse_book_info(self, response, short_name):
-        # Get book's full name and author
-        loader = ItemLoader(item=BookInfo(), response=response)
-        # Find elements
-        loader.add_xpath(LAST_CHAPTER, BOOK_LAST_CHAPTER_PATH)
+        premium_tag = response.xpath(PREMIUM_TAG_PATH)
+        if not premium_tag:
+            # Get book's full name and author
+            loader = ItemLoader(item=BookInfo(), response=response)
+            # Find elements
+            loader.add_xpath(LAST_CHAPTER, BOOK_LAST_CHAPTER_PATH)
 
-        # Extracting data
-        page = loader.load_item()
-        last_chapter = int(page.get(LAST_CHAPTER))
-        for i in range(1, last_chapter + 1):
-            yield Request(
-                url=CHAPTER_URL.format(short_name, i),
-                callback=self.parse_chapter,
-                cb_kwargs=dict(short_name=short_name, chapter_index=i),
-                dont_filter=True
-            )
+            # Extracting data
+            page = loader.load_item()
+            last_chapter = int(page.get(LAST_CHAPTER))
+            for i in range(1, last_chapter + 1):
+                yield Request(
+                    url=CHAPTER_URL.format(short_name, i),
+                    callback=self.parse_chapter,
+                    cb_kwargs=dict(short_name=short_name, chapter_index=i),
+                    dont_filter=True
+                )
 
     def parse_chapter(self, response, short_name, chapter_index):
         yield {
